3c4d55cccacac200cc340bdb18437618d2b9a2ae
RohanRudra <rohanrudra0914@gmail.com>
2024-10-25 19:29:23 +0530

    new commit

--- ImmGen.v
+++ ImmGen.v
@@ -8,7 +8,7 @@ module ImmGen(op, instr, imm_out);
             7'b0000011: imm_out <= {{20{instr[31]}}, instr[31:20]}; //lw
             7'b0010011: imm_out <= {{20{instr[31]}}, instr[31:20]}; //addi / ori
             7'b0100011: imm_out <= {{20{instr[31]}}, instr[31:25], instr[11:7]}; //sw
-            7'b1100011: imm_out <= {{19{instr[31]}}, instr[31], instr[30:25], instr[11:8], 1'b0}; //sb type
+            7'b1100011: imm_out <= {{19{instr[31]}}, instr[31], instr[7], instr[30:25], instr[11:8], 1'b0}; //sb type
         endcase
     end
 endmodule
